6903f52754daa69b9f99ab33d95f2fc12a26f34f
Stephen <stephenvdw7777@gmail.com>
2023-03-08 21:05:18 +0200

    -

--- app/controllers/users_controller.nim
+++ app/controllers/users_controller.nim
@@ -4,7 +4,7 @@ from os import `/`
 import ../../config/nimja_conf
 
 proc user_sign_up*(ctx: Context) {.async.} =
-    # ! consider using https://nim-lang.org/docs/htmlparser.html instead of Nimja
+    # ! consider using https://nim-lang.org/docs/htmlparser.html or https://nim-lang.org/docs/filters.html instead of Nimja
     # current implementation does not allow dynamic filename insertion, especially since
     # {.compiletime.} cannot be used with {.async.}
     resp renderIndex("users" / "signup")
